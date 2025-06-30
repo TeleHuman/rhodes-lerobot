a7db6526c014d368a0daebadb5d18d574a056bb9
root <chenyou01.fan@gmail.com>
2025-06-30 14:09:21 +0800

    Update local code

--- training_scripts/train_pi0.sh
+++ training_scripts/train_pi0.sh
@@ -5,21 +5,22 @@
 # DATASET_ROOT="/data/zhangyang/huggingface_cache/hub/datasets--lerobot--aloha_mobile_cabinet"
 
 # DATASET_REPO_ID="RoboMind/tienkung_gello_1rgb_normkey"
-DATASET_REPO_ID="rm75_3rgb/pick_coffee_normkey"
-DATASET_ROOT="/gemini/space/users/loy/WAIC/rm75_3rgb/pick_coffee_normkey"
+DATASET_REPO_ID="RoboTwin/all_tasks_50ep"
+DATASET_ROOT="$HF_LEROBOT_HOME/$DATASET_REPO_ID"
 
 # Pretrained model configuration  
 POLICY_PATH="$HF_HUB_CACHE/models--lerobot--pi0"
 
 # Output directory
-OUTPUT_DIR="outputs/train_pi0/test_single_gpu_3"
+OUTPUT_DIR="outputs/train_pi0/test_single_gpu_4"
 
 # Training Parameters
 BATCH_SIZE=16
 TOTAL_STEPS=280000
-SAVE_FREQ=1000
-ACTION_CHUNK_SIZE=20
+SAVE_FREQ=10000
+ACTION_CHUNK_SIZE=50
 NUM_WORKERS=16
+USE_TENSORBOARD=false
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python lerobot/scripts/train.py \
     --dataset.repo_id=$DATASET_REPO_ID \
@@ -28,7 +29,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python lerobot/scripts/train.py \
     --dataset.wrist_transforms.enable=true \
     --policy.path=$POLICY_PATH \
     --policy.local_files_only=true \
-    --output_dir=$OUTPUT_DIR \
+    --output_dir=$OUTPUT_DIR --use_tensorboard=$USE_TENSORBOARD \
     --batch_size=$BATCH_SIZE --steps=$TOTAL_STEPS --save_freq=$SAVE_FREQ --num_workers=$NUM_WORKERS \
-    --policy.max_state_dim=64 --policy.max_action_dim=64 --policy.train_expert_from_scratch=False \
+    --policy.max_state_dim=32 --policy.max_action_dim=32 --policy.train_expert_from_scratch=False \
     --policy.chunk_size=$ACTION_CHUNK_SIZE --policy.n_action_steps=$ACTION_CHUNK_SIZE
